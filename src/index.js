935318aaf45766dad880c5735800c221ac1985e4
Abhinav Swami <abhinav.swami21@gmail.com>
2017-09-25 14:32:41 -0700

    Use React-Router v4 implementation

--- src/index.js
+++ src/index.js
@@ -4,13 +4,21 @@ import 'babel-polyfill';
 import React from 'react';
 import {Provider} from 'react-redux';
 import {render} from 'react-dom';
-import {Router, browserHistory} from 'react-router';
-import routes from './routes';
+import {Router} from 'react-router-dom';
+import {createHashHistory} from 'history';
+import Routes from './routes';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import configureStore from './store/configureStore';
+import App from './components/App';
 
 const store = configureStore();
+const history = createHashHistory();
 render(
     <Provider store={store}>
-    <Router routes={routes} history={browserHistory}/>,
+    <Router history={history}>
+        <div>
+            <App/>
+            <Routes/>
+        </div>
+    </Router>
 </Provider>, document.getElementById('app'));
