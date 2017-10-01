0322c94c045938d5fd4c1193c10c30f47554908f
Abhinav Swami <abhinav.swami21@gmail.com>
2017-09-30 19:19:07 -0700

    Update routes

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import {Route} from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 import Home from './components/common/HomePage';
 import About from './components/common/AboutPage';
-import Book from './components/BookPage';
+import Book from './components/book/BookPage';
+import BookDetailsPage from './components/book/BookDetailsPage';
 
 export default class Routes extends React.Component {
     render() {
         return (
-            <div>
+            <Switch>
                 <Route exact path="/" component={Home}></Route>
+                <Route path="/books/:id" component={BookDetailsPage}></Route>
                 <Route path="/about" component={About}></Route>
                 <Route path="/books" component={Book}></Route>
-            </div>
+                
+            </Switch>
         );
     }
 }
