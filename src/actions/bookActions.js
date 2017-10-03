245a9a65ecf66a814c4574788344b5a3e290be46
Abhinav Swami <abhinav.swami21@gmail.com>
2017-10-02 23:06:55 -0700

    Add initial state

--- src/actions/bookActions.js
+++ src/actions/bookActions.js
@@ -1,8 +1,39 @@
+import Axios from 'axios';
+// API URL
+const apiUrl = 'http://59cd51e2c80b4a001175c45f.mockapi.io/api/';
+
+// Sync action
+export const fetchBooksSuccess = (books) => {
+    return {
+        type: 'FETCH_BOOKS_SUCCESS',
+        books
+    }
+};
+
+// Async action
+
+export const fetchBooks = () => {
+    // Return a dispatcher function
+    // that dispatches an action at a later time
+    return (dispatch) => {
+        return Axios.get(apiUrl)
+        .then(response => {
+            // Dispatch another action 
+            // to consume data
+            dispatch(fetchBooksSuccess(response.data))
+        })
+        .catch(error => {
+            throw(error);
+        });
+    };
+};
+
 export const createBook = (book) => {
+    console.log('Inside createBook action');
     return {
         // unique identifier
         type: 'CREATE_BOOK',
         // Payload
-        book: book
-    }
+        book
+    };
 };
