bd0511f0c9d6b465f4174f0cb1d151b4a22c483b
Abhinav Swami <abhinav.swami21@gmail.com>
2017-10-06 21:05:18 -0700

    Refractor code

--- src/components/book/BookDetailsPage.js
+++ src/components/book/BookDetailsPage.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import BookDetails from './BookDetails';
+import * as bookActions from '../../actions/bookActions';
 
 class BookDetailsPage extends React.Component {
     constructor(props, context) {
         super(props, context);
     }
+    componentDidMount() {
+        this.props.fetchBookById(this.props.match.params.id);
+    }
     render() {
         return(
             <div>
                 <h1>Book Details Page</h1>
+                <BookDetails book ={this.props.book}/>
             </div>
         );
     }
@@ -17,12 +23,16 @@ class BookDetailsPage extends React.Component {
 const mapStateToProps = (state, ownProps) => {
     return {
         // state mapping here
+        book: state.book
     };
 }
 
 const mapDispatchToProps = (dispatch) => {
     return {
+        
         // action mapping here
+        // This dispatch will trigger the ajax request we setup in our actions
+        fetchBookById: bookId => dispatch(bookActions.fetchBookById(bookId))
     }
 }
 export default connect(mapStateToProps, mapDispatchToProps)(BookDetailsPage);
