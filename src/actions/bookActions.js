26cda3fc60b46038e2275002e802000891b2c60b
Abhinav Swami <abhinav.swami21@gmail.com>
2017-10-06 21:03:10 -0700

    Add actions

--- src/actions/bookActions.js
+++ src/actions/bookActions.js
@@ -1,39 +1,65 @@
 import Axios from 'axios';
+import * as actionTypes from './actionTypes';
 // API URL
 const apiUrl = 'http://59cd51e2c80b4a001175c45f.mockapi.io/api/';
 
 // Sync action
 export const fetchBooksSuccess = (books) => {
-    return {
-        type: 'FETCH_BOOKS_SUCCESS',
-        books
-    }
+    return {type: actionTypes.FETCH_BOOKS_SUCCESS, books}
 };
 
 // Async action
 
 export const fetchBooks = () => {
-    // Return a dispatcher function
-    // that dispatches an action at a later time
+    // Return a dispatcher function that dispatches an action at a later time
     return (dispatch) => {
-        return Axios.get(apiUrl)
-        .then(response => {
-            // Dispatch another action 
-            // to consume data
-            dispatch(fetchBooksSuccess(response.data))
+        return Axios
+            .get(apiUrl)
+            .then(response => {
+                // Dispatch another action to consume data
+                dispatch(fetchBooksSuccess(response.data));
+            })
+            .catch(error => {
+                throw(error);
+            });
+    };
+};
+
+// Sync action
+export const fetchBookByIdSuccess = (book) => {
+    return {type: actionTypes.FETCH_BOOK_BY_ID_SUCCESS, book}
+};
+
+// Async action
+export const fetchBookById = (bookId) => {
+    return (dispatch) => {
+        return Axios
+            .get(apiUrl + '/' + bookId)
+            .then(response => {
+                // Handle data with sync action
+                dispatch(fetchBookByIdSuccess(response.data));
+            })
+            .catch(error => {
+                throw(error);
+            });
+    };
+};
+
+export const createBookSuccess = (book) => {
+    return{
+        type: actionTypes.CREATE_BOOK_SUCCESS,
+        book
+    }
+};
+
+export const createBook = (book) => {
+    return (dispatch) => {
+        return Axios.post(apiUrl, book)
+        .then(response =>{
+            dispatch(createBookSuccess(response.data))
         })
         .catch(error => {
             throw(error);
         });
-    };
-};
-
-export const createBook = (book) => {
-    console.log('Inside createBook action');
-    return {
-        // unique identifier
-        type: 'CREATE_BOOK',
-        // Payload
-        book
     };
 };
